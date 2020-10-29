bebab61e639d5f19a364610ab365843a38f044fd
Mojek <rychlym@atlas.cz>
2020-10-29 21:19:19 +0100

    upgrade to the .NET.Core 3.1

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -26,13 +27,13 @@ namespace WebApi
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddResponseCaching();
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
+			if (env.ApplicationName == Environments.Development)
 			{
 				app.UseDeveloperExceptionPage();
 			}
@@ -44,7 +45,6 @@ namespace WebApi
 
 			app.UseHttpsRedirection();
 			app.UseResponseCaching();
-			app.UseMvc();
 		}
 	}
 }
